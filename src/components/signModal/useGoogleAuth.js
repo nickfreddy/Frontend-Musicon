afb13868be8b91aadb45adfb08110f8b6ec21e49
gedesurya125 <gedesurya125@gmail.com>
2021-09-02 15:39:22 +0800

    some changes in api

--- src/components/signModal/useGoogleAuth.js
+++
@@ -1,9 +0,0 @@
-import { useGoogleLogin } from 'react-google-login';
-
-const clientID = `1031799664857-gtbejiap99fsrl5v6pdnhjp248cockqt.apps.googleusercontent.com`;
-
-const useGoogleAuth = () => {
-  const onSuccess =(res) => {
-
-  }
-}
