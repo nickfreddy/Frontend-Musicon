888516a3c0da5eb036482067eadd1f6d1dab0df5
gedesurya125 <gedesurya125@gmail.com>
2021-09-03 19:59:39 +0800

    finishing push notification

--- src/components/SocketIOClientComponent.jsx
+++ src/components/SocketIOClientComponent.jsx
@@ -3,14 +3,32 @@ import socketIOClient from 'socket.io-client';
 import { sourceUrl } from '../redux/Api/setupAPI';
 import { useDispatch } from 'react-redux';
 import { setNotificationAction } from '../redux/actions/notificationAction';
+import addNotification from 'react-push-notification';
 
 const SocketIOClientComponent = () => {
   const dispatch = useDispatch();
+
+  const handleAddNotification = (title, message, icon) => {
+    addNotification({
+      native: true, // when using native, your OS will handle theming.
+      title,
+      message,
+      theme: 'darkblue',
+      icon,
+      vibrate: 2,
+    });
+  }
 
   useEffect(() => {
     const socket = socketIOClient(sourceUrl);
     socket.on('newRating', data => {
       dispatch(setNotificationAction(data));
+      console.log('DATA DARI PUSH', data);
+      const user_id = localStorage.getItem('user_id');
+      if(data.playlist.author._id === user_id){
+        handleAddNotification("You got a new rating", `${data.ratedBy.username} rated ${data.rating} to your ${data.playlist.playlistTitle}.`,data.ratedBy.photo)
+
+      }
     })
 
     return () => {
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -6,6 +6,8 @@ import { openModalAuthAction } from '../redux/actions/modalAction';
 import { openCreatePlaylistModalAction } from '../redux/actions/modalAction';
 import { Button, Snackbar } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
+import addNotification from 'react-push-notification';
+import sampleIcon from '../assets/img/facebookProfile.jpg'
 
 const TestPage = () => {
   const [openSnack, setOpenSnack] = useState(false);
@@ -27,12 +29,25 @@ const TestPage = () => {
   const handleOpenSnack = () => {
     setOpenSnack(true)
   }
+
+
+  const handleAddNotification = (title, message) => {
+    addNotification({
+      title,
+      message,
+      theme: 'darkblue',
+      icon: sampleIcon,
+      native: true // when using native, your OS will handle theming.
+    });
+  }
   return (
     <div>
       {/** SURYA */}
       <Button variant="contained" color="primary" onClick={handleOpenAuthModal}>Open Auth Modal</Button>
       <Button variant="contained" color="primary" onClick={handleOpenCreatePlaylistModal}>Open Create Playlist</Button>
       <Button variant="contained" color="secondary" onClick={handleOpenSnack}>Open Snack</Button>
+      <Button variant="outlined" color="primary" onClick={() => handleAddNotification("manggo", "manggo is enak")}>Open Notification</Button>
+      <Button variant="outlined" color="primary" onClick={() => handleAddNotification("nanas", "nanassnsns is good")}>Open Notification 2</Button>
 
       <Snackbar open={openSnack} autoHideDuration={4000} onClose={handleCloseSnack}>
         <Alert onClose={handleCloseSnack} severity="success">
