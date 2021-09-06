095ef5dfa8e804b91c2938b99d2ee816855bc968
gedesurya125 <gedesurya125@gmail.com>
2021-09-06 13:33:48 +0800

    enhance artist page

--- src/redux/actions/artistAlbumAction.js
+++ src/redux/actions/artistAlbumAction.js
@@ -4,4 +4,4 @@ export const getArtistAlbumAction = (artistId) => ({type: type.GET_ARTIST_ALBUM,
 export const setArtistAlbumAction = (data) => ({type: type.SET_ARTIST_ALBUM, payload: data});
 export const setLoadingArtistAlbumAction = () => ({type: type.SET_LOADING_ARTIST_ALBUM});
 export const unsetLoadingArtistAlbumAction = () => ({type: type.UNSET_LOADING_ARTIST_ALBUM});
-export const resetLoadingArtistAlbumAction = () => ({type: type.RESET_ARTIST_ALBUM});
+export const resetArtistAlbumAction = () => ({type: type.RESET_ARTIST_ALBUM});
