99a3914b0fd285f18aa81e5ed4e61f3f15d61a94
gedesurya125 <gedesurya125@gmail.com>
2021-09-09 14:05:56 +0800

    switch back to tmp server

--- src/redux/Api/setupAPI.js
+++ src/redux/Api/setupAPI.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-export const sourceUrl = "https://musicon.gabatch13.my.id";
-// export const sourceUrl = "https://musicon-tmp.gabatch13.my.id"; //USED
-
+// export const sourceUrl = "https://musicon.gabatch13.my.id";
+export const sourceUrl = "https://musicon-tmp.gabatch13.my.id"; //USED
 // export const sourceUrl = "https://18.141.224.217"
-
 export const sourceUrlV2 = "https://musicon-tmp.gabatch13.my.id";
 
 export const musiconAPI = axios.create({
